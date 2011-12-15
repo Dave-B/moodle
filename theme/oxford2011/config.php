823bdc84f8429afc9cef38b9105c23c267b251e8
David Balch <david.balch@conted.ox.ac.uk>
2011-12-15 11:48:07 +0000

    Add Oxford theme, incorporating 'MoOdalis Tango' icons.

---
+++ theme/oxford2011/config.php
@@ -0,0 +1,63 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Configuration for TALL's University of Oxford theme.
+ *
+ * Based on Moodle's Standard theme
+ *
+ * @author    David Balch <david.balch@conted.ox.ac.uk>
+ * @copyright 2011 University of Oxford
+ * @license   All rights reserved
+ */
+
+$THEME->name = 'oxford2011';
+$THEME->parents = array('base');
+$THEME->sheets = array(
+    'core',     /** Must come first**/
+    'admin',
+    'blocks',
+    'calendar',
+    'course',
+    'user',
+    'dock',
+    'grade',
+    'message',
+    'modules',
+    'question',
+    'oxford2011'
+);
+$THEME->enable_dock = true;
+
+$THEME->layouts = array(
+    // Standard layout with blocks, this is recommended for most pages with general information
+    'standard' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    // The site home page.
+    'frontpage' => array(
+        'file' => 'frontpage.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+);
+
+// Set the frameset height on this theme
+$THEME->resourceframesize = 104;
+
