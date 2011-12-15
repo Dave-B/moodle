8a7f8da3fd6fad28a573ced18139a00641e2d62a
David Balch <david.balch@conted.ox.ac.uk>
2011-12-15 11:44:00 +0000

    Add 'Unmarked' course format.

---
+++ course/format/unmarked/config.php
@@ -0,0 +1,12 @@
+<?php
+//
+// Optional course format configuration file
+//
+// This file contains any specific configuration settings for the
+// format.
+//
+// The default blocks layout for this course format:
+    $format['defaultblocks'] = 'participants,calendar_upcoming,recent_activity,'.
+                               'activity_modules,'.
+                               'search_forums,blog_menu,admin';
+// To reinstate: oxref
---
+++ course/format/unmarked/lib.php
@@ -0,0 +1,93 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains general functions for the course format Unmarked
+ *
+ * @since 2.0
+ * @package moodlecore
+ * @author Sam Hemelryk, David Balch
+ * @copyright 2009 Sam Hemelryk, 2011 University of Oxford
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+/**
+ * Indicates this format uses sections.
+ *
+ * @return bool Returns true
+ */
+function callback_unmarked_uses_sections() {
+    return true;
+}
+
+/**
+ * Used to display the course structure for a course where format=unmarked
+ *
+ * This is called automatically by {@link load_course()} if the current course
+ * format = weeks.
+ *
+ * @param array $path An array of keys to the course node in the navigation
+ * @param stdClass $modinfo The mod info object for the current course
+ * @return bool Returns true
+ */
+function callback_unmarked_load_content(&$navigation, $course, $coursenode) {
+    return $navigation->load_generic_course_sections($course, $coursenode, 'unmarked');
+}
+
+/**
+ * The string that is used to describe a section of the course
+ * e.g. Topic, Week...
+ *
+ * @return string
+ */
+function callback_unmarked_definition() {
+    return get_string('unmarked');
+}
+
+/**
+ * The GET argument variable that is used to identify the section being
+ * viewed by the user (if there is one)
+ *
+ * @return string
+ */
+function callback_unmarked_request_key() {
+    return 'unmarked';
+}
+
+function callback_unmarked_get_section_name($course, $section) {
+    // We can't add a node without any text
+    if (!empty($section->name)) {
+        return $section->name;
+    } else if ($section->section == 0) {
+        return get_string('section0name', 'format_unmarked');
+    } else {
+        return get_string('unmarked', 'format_unmarked').' '.$section->section;
+    }
+}
+
+/**
+ * Declares support for course AJAX features
+ *
+ * @see course_format_ajax_support()
+ * @return stdClass
+ */
+function callback_unmarked_ajax_support() {
+    $ajaxsupport = new stdClass();
+    $ajaxsupport->capable = true;
+    $ajaxsupport->testedbrowsers = array('MSIE' => 6.0, 'Gecko' => 20061111, 'Safari' => 531, 'Chrome' => 6.0);
+    return $ajaxsupport;
+}
