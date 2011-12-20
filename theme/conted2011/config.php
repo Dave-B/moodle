19825e99467653d9ac56530f1870ae366cbbe702
David Balch <david.balch@conted.ox.ac.uk>
2011-12-20 16:16:34 +0000

    Add conted2011 theme, based on oxford2011.

---
+++ theme/conted2011/config.php
@@ -0,0 +1,48 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Configuration for ContEd's Moodle theme.
+ *
+ * For full information about creating Moodle themes, see:
+ *  http://docs.moodle.org/dev/Themes_2.0
+ *
+ * @package   moodlecore
+ * @copyright 2010 Sam Hemelryk
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$THEME->name = 'conted2011';
+$THEME->parents = array('oxford2011', 'base');
+
+
+$THEME->layouts = array(
+    // Standard layout with blocks, this is recommended for most pages with general information
+    'standard' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    // The site home page.
+    'frontpage' => array(
+        'file' => 'frontpage.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+);
+
+$THEME->enable_dock = true;
