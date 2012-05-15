c22418453f5acf0a7f3bddf6d84af9256c563aad
David Balch <david.balch@conted.ox.ac.uk>
2012-05-15 11:53:48 +0100

    Split list of cookies from the privacy page that displays them, for more flexibility.

---
+++ cookielist.php
@@ -0,0 +1,39 @@
+<?php
+$cookies['local'] = Array(
+                  Array(
+                         'name' => 'MOODLEID1_',
+                         'title' => 'Moodle username',
+                         'description' => "Remembers your username for the login form, so you don't have to type it in every time.",
+                         'more_info' => 'Expires two months after creation/update.',
+                         'consent' => null,
+                         'permanent' => false
+                       ),
+                  Array(
+                         'name' => 'MoodleSession',
+                         'title' => 'Moodle session',
+                         'description' => 'Required to keep you logged in whilst using the site.',
+                         'more_info' => 'Expires when you close your browser.',
+                         'consent' => null,
+                         'permanent' => false
+                       )
+                );
+
+$cookies['other'] = Array(
+                  Array(
+                         'name' => 'PREF',
+                         'title' => 'YouTube preferences',
+                         'description' => 'Stores your YouTube preferences',
+                         'more_info' => 'Expires 10 years after creation/update.',
+                         'consent' => null,
+                         'permanent' => false
+                       ),
+                  Array(
+                         'name' => 'VISITOR_INFO1_LIVE',
+                         'title' => 'YouTube analytics',
+                         'description' => 'Supports anonymous video performance analysis',
+                         'more_info' => 'Expires 7 months after creation/update.',
+                         'consent' => null,
+                         'permanent' => false
+                       )
+                );
+?>
--- privacy.php
+++ privacy.php
@@ -2,6 +2,33 @@
 
 require_once("config.php");
 
+include_once('cookielist.php');
+function cookietable($cookies, $category) {
+    $tablestart = '<table class="generaltable">
+        <thead>
+            <tr>
+                <th>Cookie</th>
+                <th>Title</th>
+                <th>Purpose</th>
+                <th>More information</th>
+            </tr>
+        </thead>
+        <tbody>';
+    $tableend = '</tbody>
+    </table>';
+
+    $tablerows = '';
+    foreach ($cookies[$category] as $cookie) {
+        $tablerows .= '<tr><td>'.$cookie['title'].'</td>'.
+                        '<td>'.$cookie['name'].'</td>'.
+                        '<td>'.$cookie['description'].'</td>'.
+                        '<td>'.$cookie['more_info'].'</td>'.
+                      "</tr>\n";
+    }
+
+    return $tablestart.$tablerows.$tableend;
+}
+
 $PAGE->set_context(get_context_instance(CONTEXT_SYSTEM));
 $PAGE->set_url('/privacy.php');
 
@@ -18,7 +45,7 @@ echo $OUTPUT->heading($title);
 echo $OUTPUT->box_start('generalbox', 'intro');
 ?>
 
-<p>This policy explains what types of personal information will be gathered when you visit the <i><?php echo $SITE->fullname; ?></i> website, and how this information will be used.</p>
+<p>This policy explains what types of personal information will be gathered when you use the <i><?php echo $SITE->fullname; ?></i> website, and how this information will be used.</p>
 
 <p>Please note that although this website provides links to other websites, this policy only applies to the <i><?php echo $SITE->fullname; ?></i> webpages.</p>
 
@@ -26,40 +53,7 @@ echo $OUTPUT->box_start('generalbox', 'intro');
 <p>Usually we will have already loaded your name and email address to create your account; if we have not already created your account, you will need to supply that information to access the site and courses. On some parts of the website, you may be asked to provide additional personal information in order to facilitate the functioning of the site.</p>
 <p>By supplying this information you are consenting to the University holding and using it for the purposes for which it was provided. Information provided will be kept for as long as is necessary to fulfil that purpose. Personal information collected and/or processed by the University is held in accordance with the provisions of the Data Protection Act 1998.</p>
 
-
-<h2 id="cookies">Cookies</h2>
-
-<p>Cookies are small text files that are placed on your computer by websites that you visit. They are widely used in order to make websites work, or work more efficiently, as well as to provide information to the owners of the site. For more information about cookies you can visit <a href="http://www.allaboutcookies.org">http://www.allaboutcookies.org</a>.</p>
-
-The following table explains the cookies we use and why.
-
-<table class="generaltable">
-    <thead>
-        <tr>
-            <th>Cookie</th>
-            <th>Name</th>
-            <th>Purpose</th>
-            <th>More information</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Moodle username</td>
-            <td>MOODLEID1_</td>
-            <td>Remembers your username for the login form, so you don't have to type it in every time.</td>
-            <td>Expires after two months.</td>
-        </tr>
-        <tr>
-            <td>Moodle session</td>
-            <td>MoodleSession</td>
-            <td>Required to keep you logged in whilst using the site.</td>
-            <td>Expires when you close your browser.</td>
-        </tr>
-    </tbody>
-</table>
-
-
-<h2>How the information collected is used</h2>
+<h3>How the information collected is used</h3>
 
 <p>Personal information provided by you will only be used for the purpose stated when the information is collected. The personal information that you provide when you register to use the website may be used:</p>
 <ul>
@@ -73,6 +67,19 @@ The following table explains the cookies we use and why.
 
 <p>Any queries or concerns about the privacy of this website should be sent to the <a href="mailto:data.protection@admin.ox.ac.uk?subject=<?php echo $CFG->wwwroot; ?>+Privacy+policy">Data Protection Officer</a>.</p>
 
+
+<h2 id="cookies">Cookies</h2>
+
+<p>Cookies are small text files that are placed on your computer by websites that you visit. They are widely used in order to make websites work, or work more efficiently, as well as to provide information to the owners of the site. For more information about cookies you can visit <a href="http://www.allaboutcookies.org">http://www.allaboutcookies.org</a>.</p>
+
+<p>The following table explains the cookies we use and why.</p>
+
+<?php echo cookietable($cookies, 'local'); ?>
+
+<p>Some pages may embed content from other websites, e.g. YouTube, which can create their own cookies.</p>
+
 <?php
+echo cookietable($cookies, 'other');
+
 echo $OUTPUT->box_end();
 echo $OUTPUT->footer();
