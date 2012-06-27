a3511cddd76b1ecaa7c95d1fffdff4ca1a813378
David Balch <david.balch@conted.ox.ac.uk>
2012-06-27 14:52:46 +0100

    Fix version file for talltopics course format.

--- course/format/topicstall/version.php
+++ course/format/topicstall/version.php
@@ -27,4 +27,4 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2012061700;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2012061700;        // Requires this Moodle version.
-$plugin->component = 'format_topics';    // Full name of the plugin (used for diagnostics).
+$plugin->component = 'format_topicstall';    // Full name of the plugin (used for diagnostics).
