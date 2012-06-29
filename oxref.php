0cad2251cda4e0022c043742ab27bcd62c3a996f
David Balch <david.balch@conted.ox.ac.uk>
2012-06-29 09:49:07 +0100

    Require login to access oxref page.

--- oxref.php
+++ oxref.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once("config.php");
+require_login($cm->course, true, $cm);
 
 $redirect = optional_param('redirect', NULL, PARAM_TEXT);
 $dest     = optional_param('dest', '', PARAM_TEXT);
