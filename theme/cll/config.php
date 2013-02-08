0d3ddf43acaebceb589c5ec2452ddb28b89c8ac1
David Balch <david.balch@conted.ox.ac.uk>
2013-02-08 14:48:36 +0000

    Add new CLL ('Changing the Learning Landscape') Moodle theme.

---
+++ theme/cll/config.php
@@ -0,0 +1,58 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Configuration for Moodle's standard theme.
+ *
+ * This theme is the default theme within Moodle 2.0, it builds upon the base theme
+ * adding only CSS to create the simple look and feel Moodlers have come to recognise.
+ *
+ * For full information about creating Moodle themes, see:
+ *  http://docs.moodle.org/dev/Themes_2.0
+ *
+ * @package   moodlecore
+ * @copyright 2010 Sam Hemelryk
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$THEME->name = 'cll';
+$THEME->parents = array('standard', 'base');
+$THEME->sheets = array(
+    'cll',
+);
+$THEME->enable_dock = true;
+
+$THEME->layouts = array(
+    // Most backwards compatible layout without the blocks - this is the layout used by default
+    'base' => array(
+        'file' => 'general.php',
+        'regions' => array(),
+    ),
+    // Standard layout with blocks, this is recommended for most pages with general information
+    'standard' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    // Main course page
+    'course' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+        'options' => array('langmenu'=>true),
+    ),
+);
