2863a6a1dfb296d04d113b0869aa88bf86867079
David Balch <david.balch@conted.ox.ac.uk>
2013-05-01 10:33:54 +0100

    Add privacy notes for Piwik webstats.

--- privacy.php
+++ privacy.php
@@ -2,33 +2,6 @@
 
 require_once("config.php");
 
-include_once('cookielist.php');
-function cookietable($cookies, $category) {
-    $tablestart = '<table class="generaltable">
-        <thead>
-            <tr>
-                <th>Cookie</th>
-                <th>Title</th>
-                <th>Purpose</th>
-                <th>More information</th>
-            </tr>
-        </thead>
-        <tbody>';
-    $tableend = '</tbody>
-    </table>';
-
-    $tablerows = '';
-    foreach ($cookies[$category] as $cookie) {
-        $tablerows .= '<tr><td>'.$cookie['title'].'</td>'.
-                        '<td>'.$cookie['name'].'</td>'.
-                        '<td>'.$cookie['description'].'</td>'.
-                        '<td>'.$cookie['more_info'].'</td>'.
-                      "</tr>\n";
-    }
-
-    return $tablestart.$tablerows.$tableend;
-}
-
 $PAGE->set_context(get_context_instance(CONTEXT_SYSTEM));
 $PAGE->set_url('/privacy.php');
 
@@ -41,17 +14,24 @@ $PAGE->navbar->add($title);
 echo $OUTPUT->header();
 echo $OUTPUT->heading($title);
 
+$webstatsenabled = strpos(get_config('core', 'additionalhtmlfooter'), 'webstats.conted') ? true : false;
 
 echo $OUTPUT->box_start('generalbox', 'intro');
 ?>
-
 <p>This policy explains what types of personal information will be gathered when you use the <i><?php echo $SITE->fullname; ?></i> website, and how this information will be used.</p>
 
-<p>Please note that although this website provides links to other websites, this policy only applies to the <i><?php echo $SITE->fullname; ?></i> webpages.</p>
+<p>Please note that although this website provides links to other websites, this policy only applies to <?php echo $SITE->fullname; ?>.</p>
 
 <h2>Information collected</h2>
 <p>Usually we will have already loaded your name and email address to create your account; if we have not already created your account, you will need to supply that information to access the site and courses. On some parts of the website, you may be asked to provide additional personal information in order to facilitate the functioning of the site.</p>
 <p>By supplying this information you are consenting to the University holding and using it for the purposes for which it was provided. Information provided will be kept for as long as is necessary to fulfil that purpose. Personal information collected and/or processed by the University is held in accordance with the provisions of the Data Protection Act 1998.</p>
+
+<?php if ($webstatsenabled) { ?>
+<p>Like most website operators, we collect non-personally-identifying information of the sort that web browsers and servers typically make available, such as the browser type, language preference, referring site, and the date and time of each visitor request. Our purpose in collecting non-personally identifying information is to better understand how visitors use the website. From time to time, we may release non-personally-identifying information in the aggregate, e.g., by publishing a report on trends in the usage of its website.</p>
+
+</p><?php echo $SITE->fullname; ?> also collects potentially personally-identifying information like Internet Protocol (IP) addresses. We do not use such information to identify its visitors, however, and does not disclose such information, other than under the same circumstances that it uses and discloses personally-identifying information, as described below.</p>
+<?php } ?>
+
 
 <h3>How the information collected is used</h3>
 
@@ -74,12 +54,44 @@ echo $OUTPUT->box_start('generalbox', 'intro');
 
 <p>The following table explains the cookies we use and why.</p>
 
-<?php echo cookietable($cookies, 'local'); ?>
+<table class="generaltable">
+    <thead>
+        <tr>
+            <th>Cookie</th>
+            <th>Title</th>
+            <th>Purpose</th>
+            <th>More information</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr><td>Moodle username</td><td>MOODLEID1_</td><td>Remembers your username for the login form, so you don't have to type it in every time.</td><td>Expires two months after creation/update.</td></tr>
+        <tr><td>Moodle session</td><td>MoodleSession</td><td>Required to keep you logged in whilst using the site.</td><td>Expires when you close your browser.</td></tr>
+        <?php if ($webstatsenabled) { ?>
+        <tr><td>Piwik</td><td>_pk_ref, _pk_cvar, _pk_id, _pk_ses</td><td>Web analytics (website audience statistics).</td><td>Expires up to 2 years after creation/update.</td></tr>
+        <?php } ?>
+    </tbody>
+</table>
+
+<?php if ($webstatsenabled) { ?>
+<iframe style="border: 0;" width="100%"  src="http://webstats.conted.ox.ac.uk/index.php?module=CoreAdminHome&action=optOut&language=en"></iframe>
+<?php } ?>
 
 <p>Some pages may embed content from other websites, e.g. YouTube, which can create their own cookies.</p>
-
+<table class="generaltable">
+    <thead>
+        <tr>
+            <th>Cookie</th>
+            <th>Title</th>
+            <th>Purpose</th>
+            <th>More information</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr><td>YouTube preferences</td><td>PREF</td><td>Stores your YouTube preferences.</td><td>Expires 10 years after creation/update.</td></tr>
+        <tr><td>YouTube analytics</td><td>VISITOR_INFO1_LIVE</td><td>Supports video performance analysis.</td><td>Expires 7 months after creation/update.</td></tr>
+    </tbody>
+</table>
 <?php
-echo cookietable($cookies, 'other');
 
 echo $OUTPUT->box_end();
 echo $OUTPUT->footer();
