61ecf39b8685d50c15408a968c254a363746e704
David Balch <david.balch@conted.ox.ac.uk>
2013-08-20 15:22:08 +0100

    Add Units format block config.

---
+++ course/format/units/config.php
@@ -0,0 +1,12 @@
+<?php
+//
+// Optional course format configuration file
+//
+// This file contains any specific configuration settings for the
+// format.
+//
+// The default blocks layout for this course format:
+    $format['defaultblocks'] = 'participants,calendar_month,recent_activity,'.
+                               'activity_modules,blog_menu,oxref,'.
+                               'calendar_upcoming,search_forums';
+//
