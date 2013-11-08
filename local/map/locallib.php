97369e9403d16f0dff1dc43008743855daec9cfb
David Balch <david.balch@conted.ox.ac.uk>
2013-11-08 10:08:40 +0000

    Better code for database preset (saved here for convenience).

--- local/map/locallib.php
+++ local/map/locallib.php
@@ -123,28 +123,70 @@ function map_receive_markers ($mapid) {
 
 == List header ==
 <div id="datamap" style="width: 640px; height: 320px;">Loading map...</div>
+<table id="mod-data-map-template"><thead><tr>
+<th>Delete</th>
+<th>Title</th>
+<th>Location</th>
+<th>Description</th>
+<th>Controls</th>
+</tr></thead><tbody>
+
 
 == List repeated entry ==
-<textarea class="geojson" style="display: none;">{"type":"Feature", "id":"[[Title]]", "geometry":{ "type":"Point", "coordinates":[[[Location]]]}, "properties":{ "name":"[[Title]]", "info":"[[Description]]"}},</textarea>
+<tr class="data-entry">
+<td class="template-field cell c0 firstcol">##delcheck##</td>
+<td class="template-token cell c1">[[Title]]</td>
+<td class="template-token cell c2">[[Location]]</td>
+<td class="template-token cell c3 lastcol">[[Description]]</td>
+<td class="controls template-field cell c4 lastcol">##edit##  ##more##  ##delete##  ##approve##  ##disapprove##  ##export##</td>
+</tr>
+
+== List footer ==
+</tbody></table>
+
+
+== Single template ==
+<div class="defaulttemplate">
+<h3>[[Title]]</h3>
+<table class="mod-data-default-template">
+<tbody>
+<tr class="data-entry">
+<td class="template-token cell c0 firstcol"[[Location]]</td>
+<td class="template-token cell c1">[[Description]]</td>
+<td class="controls template-field cell c2 lastcol">##edit##  ##more##  ##delete##  ##approve##  ##disapprove##  ##export##</td>
+</tr>
+</tbody>
+</table>
+</div>
+
+
 
 == javascript ==
 
 Y.on("domready", function () {
     if (Y.one("#datamap")) {
+        // Init map
         M.local_map.map.maps["datamap"] = M.local_map.map.addmap("datamap", {center: [58.14288114185, -7.2773426771164], zoom: 1});
-        var json_text = '{"type":"Feature Collection","features":[';
-        Y.all("textarea.geojson").each(function (taskNode) {
-            json_text += taskNode.get('innerHTML');
+        // Add entries
+        Y.all("#mod-data-map-template .data-entry").each(function (taskNode) {
+            var title = taskNode.getElementsByTagName('td').item(1).get('innerHTML');
+            var coords = taskNode.getElementsByTagName('td').item(2).get('innerHTML').split(',');
+            var desc = taskNode.getElementsByTagName('td').item(3).get('innerHTML');
+            var popuptext = '<h3>'+title+'</h3><div>'+desc+'</div>';
+            var boxtext = '<div class ="box generalbox">'+popuptext+'</div>';
+            L.marker([coords[0], coords[1]], {title:title}).addTo(M.local_map.map.maps["datamap"]).on('click', function(e) {
+                Y.one("#datapanel").set('innerHTML', boxtext);
+            });
         });
-        geo = JSON.parse(json_text.substr(0, json_text.length -1) + ']}');
-        console.log(geo);
-
-        L.geoJson(geo, {
-        onEachFeature: function (feature, layer) {
-            var popuptext = '<h3>'+feature.properties.name+'</h3><div>'+feature.properties.info+'</div>';
-            layer.bindPopup(popuptext);
-        }}).addTo(M.local_map.map.maps["datamap"]);
+        // Hide table and buttons
+        Y.one("#datapanel").insert('<div id="datapanelvis"><input id="showdata" type="button" value="Show data table" /><input id="hidedata" type="button" value="Hide data table" />', 'after');
+        var dataelements = Y.all('#mod-data-map-template, #hidedata, #checkall, #checknone, input.form-submit');
+        var notdataelements = Y.one('#showdata');
+        Y.one('#hidedata').on('click', function() {dataelements.hide();notdataelements.show()});
+        Y.one('#showdata').on('click', function() {dataelements.show();notdataelements.hide()});
+        dataelements.hide();notdataelements.show()
     }
 });
+
 
 */
