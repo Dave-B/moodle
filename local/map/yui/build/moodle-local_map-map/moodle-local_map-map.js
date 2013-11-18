1f3bae551e8dc07e35021eed437e4f4bd4367452
David Balch <david.balch@conted.ox.ac.uk>
2013-11-18 11:01:07 +0000

    Better YUI loading in Data preset.

--- local/map/yui/build/moodle-local_map-map/moodle-local_map-map.js
+++ local/map/yui/build/moodle-local_map-map/moodle-local_map-map.js
@@ -1,27 +1,29 @@
 YUI.add('moodle-local_map-map', function (Y, NAME) {
 
-// Module moodle-local_map
-
+// Module moodle-local_map-map
 M.local_map = M.local_map || {};
-var NS = M.local_map = {};
+var NS = M.local_map;
 
-NS.init = function() {
+NS.init = function(callback) {
     Y.Get.css('http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.css', function (err) {
         if (err) {
-            return;
         } else {
             Y.Get.js('http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.js', function (err) {
                 if (err) {
-                    return;
+                } else {
+                    NS.L = L;
+                    NS.maps = {};
+                    // In page, create callback containing `map = M.local_map.addmap(id, opts);`
+                    if (callback) {
+                        callback();
+                    }
                 }
-                NS.L = L;
-                NS.maps = {};
-                // On page, use `map = M.local_map.addmap(id, opts);`
             });
         }
     });
 },
 NS.addmap = function(targetid, opts) {
+    L = NS.L;
     // Create a map in the "map" div, set the view to a given place and zoom
     opts = opts || {center: [0, 0], zoom: 0};
     var map = L.map(targetid, opts);
@@ -40,7 +42,7 @@ NS.reversegeocode = function(lat, lon, callback_apply) {
                 var parsedResponse;
                 try {
                     parsedResponse = Y.JSON.parse(r.responseText);
-					callback_apply(parsedResponse);
+                    callback_apply(parsedResponse);
                 }
                 catch (e) {
                     return;
