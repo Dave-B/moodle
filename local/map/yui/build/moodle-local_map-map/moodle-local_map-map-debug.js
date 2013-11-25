38d1a09a4768b98ed83cffb5585196572dab33ed
David Balch <david.balch@conted.ox.ac.uk>
2013-11-25 10:19:25 +0000

    Fix YIU requirements for reverse geocode lookup.

--- local/map/yui/build/moodle-local_map-map/moodle-local_map-map-debug.js
+++ local/map/yui/build/moodle-local_map-map/moodle-local_map-map-debug.js
@@ -8,15 +8,15 @@ NS.init = function(callback) {
     Y.log('moodle-local_map-map: start init');
     Y.Get.css('http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.css', function (err) {
         if (err) {
-            Y.log(err, 'error', 'leaflet-for-yui');
+            Y.log('Could not load leaflet.css', 'error', 'moodle-local_map-map');
         } else {
             Y.Get.js('http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.js', function (err) {
                 if (err) {
-                    Y.log(error, 'error', 'leaflet-for-yui');
+                    Y.log('Could not load leaflet.js', 'error', 'moodle-local_map-map');
                 } else {
                     NS.L = L;
                     NS.maps = {};
-                    Y.log('moodle-local_map-map: Leaflet initialised');
+                    Y.log('moodle-local_map-map: Leaflet initialised', 'info', 'moodle-local_map-map');
                     // In page, create callback containing `map = M.local_map.addmap(id, opts);`
                     if (callback) {
                         callback();
@@ -49,16 +49,16 @@ NS.reversegeocode = function(lat, lon, callback_apply) {
                     callback_apply(parsedResponse);
                 }
                 catch (e) {
-                    Y.log(error, 'reversegeocode: JSON Parse failed', 'moodle-local_map-map'); // TODO Check/correct this use of error logging
+                    Y.log('reversegeocode: JSON Parse failed', 'error', 'moodle-local_map-map');
                     return;
                 }
             },
             failure : function () {
-                Y.log(error, 'reversegeocode: Lookup failed', 'moodle-local_map-map');
+                Y.log('reversegeocode: Lookup failed', 'error', 'moodle-local_map-map');
             }
         }
     });
 };
 
 
-}, '@VERSION@', {"requires": ["get"]});
+}, '@VERSION@', {"requires": ["get", "io", "json-parse"]});
--- local/map/yui/src/map/js/map.js
+++ local/map/yui/src/map/js/map.js
@@ -6,15 +6,15 @@ NS.init = function(callback) {
     Y.log('moodle-local_map-map: start init');
     Y.Get.css('http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.css', function (err) {
         if (err) {
-            Y.log(err, 'error', 'leaflet-for-yui');
+            Y.log('Could not load leaflet.css', 'error', 'moodle-local_map-map');
         } else {
             Y.Get.js('http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.js', function (err) {
                 if (err) {
-                    Y.log(error, 'error', 'leaflet-for-yui');
+                    Y.log('Could not load leaflet.js', 'error', 'moodle-local_map-map');
                 } else {
                     NS.L = L;
                     NS.maps = {};
-                    Y.log('moodle-local_map-map: Leaflet initialised');
+                    Y.log('moodle-local_map-map: Leaflet initialised', 'info', 'moodle-local_map-map');
                     // In page, create callback containing `map = M.local_map.addmap(id, opts);`
                     if (callback) {
                         callback();
@@ -47,12 +47,12 @@ NS.reversegeocode = function(lat, lon, callback_apply) {
                     callback_apply(parsedResponse);
                 }
                 catch (e) {
-                    Y.log(error, 'reversegeocode: JSON Parse failed', 'moodle-local_map-map'); // TODO Check/correct this use of error logging
+                    Y.log('reversegeocode: JSON Parse failed', 'error', 'moodle-local_map-map');
                     return;
                 }
             },
             failure : function () {
-                Y.log(error, 'reversegeocode: Lookup failed', 'moodle-local_map-map');
+                Y.log('reversegeocode: Lookup failed', 'error', 'moodle-local_map-map');
             }
         }
     });
