d02fc3c76e5a6787d1f1725d2b39750c70584032
David Balch <david.balch@conted.ox.ac.uk>
2013-11-28 15:15:14 +0000

    Infomap preset layout improvements.

--- mod/data/preset/infomap_latlongmap/jstemplate.js
+++ mod/data/preset/infomap_latlongmap/jstemplate.js
@@ -2,21 +2,27 @@ YUI({
   delayUntil: 'domready'
 }).use('event', 'node', 'moodle-local_map-map', function(Y) {
   if (Y.one("#datamap")) {
+    var info = 'Click on a marker to view an entry, or add your own entry via the "Add entry" tab, above.';
     M.local_map.init(function() {
+      Y.one("#datamapinfo").set('innerHTML', info);
       M.local_map.maps["datamap"] = M.local_map.addmap("datamap", {center: [46.073, 8.437], zoom: 1});
+        // Remove del col if no rights
+        if (!Y.one("#mod-data-map-template .delcell input")) {
+          Y.all("#delhead, #mod-data-map-template .delcell").remove();
+        }
         // Add entries
         Y.all("#mod-data-map-template .data-entry").each(function (taskNode) {
-          var area = taskNode.getElementsByTagName('td').item(1).get('innerHTML');
-          var coords = taskNode.getElementsByTagName('td').item(2).get('innerHTML').split(',');
-          var desc = taskNode.getElementsByTagName('td').item(3).get('innerHTML');
+          var area = taskNode.getElementsByTagName('td').item(0).get('innerHTML');
+          var coords = taskNode.getElementsByTagName('td').item(1).get('innerHTML').split(',');
+          var desc = taskNode.getElementsByTagName('td').item(2).get('innerHTML');
           var popuptext = '<h3>'+area+'</h3><div>'+desc+'</div>';
           var closebox = '<div><a href="#" id="mapentryremove" title="Hide information">x</a></div>';
-          var boxtext = '<div>'+closebox+popuptext+'</div>';
+          var boxtext = '<div>'+popuptext+closebox+'</div>';
           L.marker([coords[0], coords[1]], {title:area}).addTo(M.local_map.maps["datamap"]).on('click', function(e) {
             Y.one("#datamapinfo").set('innerHTML', boxtext);
             Y.one("#mapentryremove").on('click', function(e) {
               e.preventDefault();
-              Y.one("#datamapinfo").set('innerHTML', '')
+              Y.one("#datamapinfo").set('innerHTML', info);
             });
           });
         });
