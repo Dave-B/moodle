f7e426672f0f044e2298454a11b42c6d48674fbb
David Balch <david.balch@conted.ox.ac.uk>
2014-09-03 16:02:12 +0100

    MDL-36874 mod_book: Add text navigation.
    Based on a patch by Matthew Cannings, this replaces Book's previous/next arrow
    image links with a choice of: no links, image links, or chapter title text links.

--- mod/book/backup/moodle2/backup_book_stepslib.php
+++ mod/book/backup/moodle2/backup_book_stepslib.php
@@ -32,7 +32,7 @@ class backup_book_activity_structure_step extends backup_activity_structure_step
     protected function define_structure() {
 
         // Define each element separated
-        $book     = new backup_nested_element('book', array('id'), array('name', 'intro', 'introformat', 'numbering', 'customtitles', 'timecreated', 'timemodified'));
+        $book     = new backup_nested_element('book', array('id'), array('name', 'intro', 'introformat', 'numbering', 'navstyle', 'customtitles', 'timecreated', 'timemodified'));
         $chapters = new backup_nested_element('chapters');
         $chapter  = new backup_nested_element('chapter', array('id'), array('pagenum', 'subchapter', 'title', 'content', 'contentformat', 'hidden', 'timemcreated', 'timemodified', 'importsrc',));
 
