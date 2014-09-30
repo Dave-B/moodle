62434b6ad73bec64e9ca90273f04d497d1f0ea11
David Balch <david.balch@conted.ox.ac.uk>
2014-09-30 12:53:57 +0100

    InfoMap: Skip markers without coords, so JS doesn't break. See #3868.

--- mod/data/preset/infomap_latlongmap/jstemplate.js
+++ mod/data/preset/infomap_latlongmap/jstemplate.js
@@ -12,19 +12,21 @@ YUI({
         }
         // Add entries
         Y.all("#mod-data-map-template .data-entry").each(function (taskNode) {
-          var area = taskNode.getElementsByTagName('td').item(0).get('innerHTML');
           var coords = taskNode.getElementsByTagName('td').item(1).get('innerHTML').split(',');
-          var desc = taskNode.getElementsByTagName('td').item(2).get('innerHTML');
-          var popuptext = '<h3>'+area+'</h3><div>'+desc+'</div>';
-          var closebox = '<div><a href="#" id="mapentryremove" title="Hide information">x</a></div>';
-          var boxtext = '<div>'+popuptext+closebox+'</div>';
-          L.marker([coords[0], coords[1]], {title:area}).addTo(M.local_map.maps["datamap"]).on('click', function(e) {
-            Y.one("#datamapinfo").set('innerHTML', boxtext);
-            Y.one("#mapentryremove").on('click', function(e) {
-              e.preventDefault();
-              Y.one("#datamapinfo").set('innerHTML', info);
-            });
-          });
+          if (coords.length == 2) {
+              var area = taskNode.getElementsByTagName('td').item(0).get('innerHTML');
+              var desc = taskNode.getElementsByTagName('td').item(2).get('innerHTML');
+              var popuptext = '<h3>'+area+'</h3><div>'+desc+'</div>';
+              var closebox = '<div><a href="#" id="mapentryremove" title="Hide information">x</a></div>';
+              var boxtext = '<div>'+popuptext+closebox+'</div>';
+              L.marker([coords[0], coords[1]], {title:area}).addTo(M.local_map.maps["datamap"]).on('click', function(e) {
+                Y.one("#datamapinfo").set('innerHTML', boxtext);
+                Y.one("#mapentryremove").on('click', function(e) {
+                  e.preventDefault();
+                  Y.one("#datamapinfo").set('innerHTML', info);
+                });
+              });
+          }
         });
         // Hide table and buttons
         Y.one("#mod-data-map-template").insertBefore('<div id="datapanelvis"><input id="showdata" type="button" value="Show data table" /><input id="hidedata" type="button" value="Hide data table" />', 'after');
